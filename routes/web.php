080d1d70f91a65cd8b5d8c6ba37616c53e68782f
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-03-10 23:12:53 -0300

    remoção de arquivos não necessários

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\FormularioController;
+use App\Http\Controllers\InputController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,10 +22,7 @@ Route::get('/welcome', function () {
 
 Route::get('/',[AuthenticatedSessionController::class,'create'])->middleware('guest');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::resource('formularios', FormularioController::class)->middleware('auth');
+Route::resource('inputs', InputController::class)->middleware('auth');
 
 require __DIR__.'/auth.php';
