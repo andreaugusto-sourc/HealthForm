6c28c965f206c15c799235918ca157b11153b213
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-07-06 01:12:44 -0300

    mudanças visuais na tela de ver respostas

--- resources/views/respostas/show.blade.php
+++ resources/views/respostas/show.blade.php
@@ -4,13 +4,14 @@
 
 @section('content')
     <section class="d-flex flex-column bg-default">
-        <h1 class="text-center text-dark w-100 display-5 fw-bold mb-3">{{$Questionario->titulo}} - Respostas</h1>
+        <h1 class="text-center text-dark w-100 display-5 fw-bold mb-3">{{$Questionario->titulo}} - Respostas dos Usuários </h1>
         @foreach ($users as $user)
             <h2 class="text-start text-dark w-100 fs-1 fw-bold mb-3">{{$user->name}}</h2>
             @foreach ($perguntas as $pergunta)
                 @foreach ($respostas as $resposta)
                     @if ($user->id == $resposta->user_id && $resposta->pergunta_id == $pergunta->id )
-                    <p class="fs-5"><strong>Pergunta do questionário: </strong> {{$pergunta->texto}}? <strong>Resposta do usuário: </strong>{{$resposta->texto}}</p>
+                    <p class="fs-5 w-100 text-start"><strong>Pergunta do questionário: </strong> {{$pergunta->texto}}?</p>
+                    <p class="fs-5 w-100 text-end"><strong>Resposta do usuário: </strong>{{$resposta->texto}}</p>
                     @endif
                 @endforeach
             @endforeach
