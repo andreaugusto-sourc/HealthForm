bc906c39a9fe09fc97f64389580b6308048ee2b1
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-08-27 01:33:37 -0300

    refactor: mudança no css

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,19 +5,21 @@
 @section('content')
 
 <section class="d-flex justify-content-between flex-wrap home">
+<a href="{{route('dashboard.posts')}}" class="text-center">
     <div class="d-flex flex-column align-items-center bg-default">
         <h1>Dashboard de Posts</h1>
         <img src="/images/multiple-users.png" alt="">
         <p class="text-center">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
-        <a href="{{route('dashboard.posts')}}"><button type="button" class="btn btn-dark btn-lg">Acessar</button></a>
     </div>
+</a>
 
+<a href="{{route('dashboard.questionarios')}}" class="text-center">
     <div class="d-flex flex-column align-items-center bg-default">
         <h1>Dashboard de Questionários</h1>
         <img src="/images/list.png" alt="Imagem de um questionário">
         <p class="text-center">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
-        <a href="{{route('dashboard.questionarios')}}"><button type="button" class="btn btn-dark btn-lg">Acessar</button></a>
     </div>
+</a>
 </section>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,19 +5,21 @@
 @section('content')
 
 <section class="d-flex justify-content-between flex-wrap home">
+<a href="{{route('posts.index')}}" class="text-center">
     <div class="d-flex flex-column align-items-center bg-default">
         <h1>Textos Motivacionais</h1>
         <img src="/images/multiple-users.png" alt="">
         <p class="text-center">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
-        <a href="{{route('posts.index')}}"><button type="button" class="btn btn-dark btn-lg">Acessar</button></a>
     </div>
+</a>
 
+<a href="{{route('questionarios.index')}}" class="text-center">
     <div class="d-flex flex-column align-items-center bg-default">
         <h1>Questionários</h1>
         <img src="/images/list.png" alt="Imagem de um questionário">
         <p class="text-center">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
-        <a href="{{route('questionarios.index')}}"><button type="button" class="btn btn-dark btn-lg">Acessar</button></a>
     </div>
+</a>
 </section>
 
 @endsection
