08c8782544546f26997897d535973d242b66f1a1
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-10-17 01:42:00 -0300

    refactor: alteração na pagina de dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,6 @@
     <div class="d-flex flex-column align-items-center bg-default">
         <h1>Dashboard de Posts</h1>
         <img src="/images/multiple-users.png" alt="">
-        <p class="text-center">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
     </div>
 </a>
 
@@ -17,7 +16,6 @@
     <div class="d-flex flex-column align-items-center bg-default">
         <h1>Dashboard de Questionários</h1>
         <img src="/images/list.png" alt="Imagem de um questionário">
-        <p class="text-center">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI.</p>
     </div>
 </a>
 </section>
