e61830665fdb02ef5d3f90f75429494c3adee6e7
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-10-20 12:40:36 -0300

    feat: adição de seeder de Categoria

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('categorias')->insert([
+            [
+                'nome' => 'Saúde e Bem-Estar',
+            ],
+            [
+                'nome' => 'Vulnerabilidade Social',
+            ],
+            [
+                'nome' => 'Insegurança Alimentar',
+            ],
+            [
+                'nome' => 'Efeitos Pós-Pandemia',
+            ],
+            [
+                'nome' => 'Inclusão e Diversidade',
+            ],
+            [
+                'nome' => 'Acesso a Auxilios Escolares',
+            ],
+            [
+                'nome' => 'Apoio Familiar e Comunitário'
+            ],
+            [
+                'nome' => 'Habitação e Infraestrutura'
+            ],
+            [
+                'nome' => 'Educação e Aprendizagem'
+            ]
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Database\Seeders\CategoriaSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(CategoriaSeeder::class);
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'admin' => 1,
+            'password'=> bcrypt('administrador'),
+        ]);
     }
 }
