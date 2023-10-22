5df5be45e6213d4245972c23655bfb014bb44152
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-10-22 03:05:11 -0300

    feat: adição de funções em models

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -10,4 +10,9 @@ class Categoria extends Model
     use HasFactory;
 
     protected $fillable = ['nome'];
+
+    public static function pegarCategorias ()
+    {
+        return Categoria::all();
+    }
 }
