833e1421ef1e5554c4df708e4830d89cef8c5c95
andreaugusto-sourc <andreaugusto1033@gmail.com>
2023-10-24 15:11:29 -0300

    refactor: modificações em views

--- resources/views/perguntas/dashboard.blade.php
+++ resources/views/perguntas/dashboard.blade.php
@@ -6,27 +6,27 @@
     <div class="col-12">
         <div class="card">
             <div class="card-header text-dark">
-                <h1>Dashboard de {{$Questionario->titulo}}</h1>
+                <h1>Dashboard de {{$questionario->titulo}}</h1>
             </div>
             <div class="card-body">
-                <a href="{{route('perguntas.create',["adicionarPergunta" => $Questionario->id])}}" class="btn btn-success btn-lg">Adicionar Pergunta</a>
+                <a href="{{route('perguntas.create',["adicionarPergunta" => $questionario->id])}}" class="btn btn-success btn-lg">Adicionar Pergunta</a>
             </div>
         </div>
     </div>
 </div>
 
-@foreach ($Perguntas as $Pergunta)
+@foreach ($questionario->perguntas as $pergunta)
 <div class="d-flex justify-content-around align-items-center m-5">
 
-    <article class="w-25 text-white text-decoration-none fs-3">{{$Pergunta->texto}}</article>
+    <article class="w-25 text-white text-decoration-none fs-3">{{$pergunta->texto}}</article>
 
-    <form action="{{route('perguntas.destroy',$Pergunta->id)}}" method="post">
+    <form action="{{route('perguntas.destroy', $pergunta->id)}}" method="post">
     @csrf
     @method('delete')
     <button type="submit" class="btn btn-danger">Deletar</button>
     </form>
 
-    <button type="button" class="btn btn-primary"><a class="text-white" href="{{route('perguntas.edit',$Pergunta->id)}}">Editar</a></button>
+    <button type="button" class="btn btn-primary"><a class="text-white" href="{{route('perguntas.edit',$pergunta->id)}}">Editar</a></button>
 </div>
 @endforeach
 
